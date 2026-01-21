52ea8ef2ac980fe0b3eae0e1218fca469e79710a
Suryakanta Mohapatra <neural.surya@gmail.com>
2026-01-21 14:49:57 -0800

    Added local model reference for evaluation

--- deepeval_with_local_llm_langchain.py
+++ deepeval_with_local_llm_langchain.py
@@ -1,7 +1,7 @@
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", message="Core Pydantic V1 functionality isn't compatible with Python 3.14 or greater")
-
+from deepeval.models import OllamaModel
 from langchain_ollama import ChatOllama
 
 llm = ChatOllama(
@@ -11,16 +11,21 @@ llm = ChatOllama(
     max_token=250
 )
 
+model = OllamaModel(
+    model="deepseek-r1:8b",
+    base_url="http://localhost:11434",
+    temperature=0
+)
 # testing LLM with actual output received from LLM call
 from deepeval.test_case import LLMTestCase
 from deepeval.metrics import AnswerRelevancyMetric
 from deepeval.evaluate import evaluate
 
-answer_relevancy_metric = AnswerRelevancyMetric()
+answer_relevancy_metric = AnswerRelevancyMetric(model=model)
 test_case = LLMTestCase(
     input = "What is the capital of Odisha?",
     actual_output=llm.invoke("What is the capital of Odisha?").content,
     retrieval_context=["The capital of Odisha is also called Temple city"]
 )
 
-evaluate(test_cases=[test_case], metrics=[answer_relevancy_metric])
+evaluate(test_cases=[test_case], metrics=[answer_relevancy_metric], identifier="local evaluation, local output")
